ba62a7f2b7f0e95e7569dcc59080edbb82ea3d2b
Pouriya <pouriya.jamshidi@gmail.com>
2022-01-29 23:05:01 +0100

    Introduce optimized font size for comments and add CSS styling for font colors

--- darker.js
+++ darker.js
@@ -2,9 +2,9 @@ const textColor = "#ccc"
 const ytTextPrimary = "--yt-spec-text-primary"
 
 try {
-    document.documentElement.style.setProperty(ytTextPrimary, textColor);
-    console.log("[+] Darker Text: Successfully changed YouTube text color")
+    document.documentElement.style.getPropertyValue(ytTextPrimary);
+    console.log("[+] Darker Text: Successfully changed YouTube text color to:", textColor)
 } catch (error) {
-    console.log("[-] Darker Text: Failed to change YouTube text color")
+    console.log("[-] Darker Text: Failed to change YouTube text color to:", textColor)
     console.log(error)
 }
